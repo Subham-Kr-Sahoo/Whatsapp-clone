03cf257c83c3e96072bb726680d1f75610596a10
Ishsubham <subhamkrsahoo@gmail.com>
2024-09-16 04:23:08 +0530

    now it works perfectly fine

---
+++ Whatsapp-Clone/MainTabView.swift
@@ -0,0 +1,20 @@
+//
+//  MainTabView.swift
+//  Whatsapp-Clone
+//
+//  Created by Subham  on 16/09/24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView{
+            
+        }
+    }
+}
+
+#Preview {
+    MainTabView()
+}
